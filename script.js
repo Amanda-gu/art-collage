cc884dbf48438ef416fe2a45475bcdc95159603c
Amanda-gu <guoj826@newschool.edu>
2026-05-16 15:13:24 -0400

    click logic

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 let showSquare = 'show'
 let mondrianSquares = document.querySelectorAll('#mondrian div')
 let resetButton = document.querySelector('#reset')
+let showReset = 'show-reset'
+let instruction = document.querySelector('p')
+let removeInstruction = 'remove-instruction'
 
 resetButton.addEventListener('click', () => {
     mondrianSquares.forEach((sq) => {
         sq.classList.remove(showSquare)
+        instruction.classList.remove(removeInstruction)
+        resetButton.classList.remove(showReset)
     })
+
 })
 
 mondrianSquares.forEach((sq) => {
     sq.addEventListener('click', () => {
         sq.classList.toggle(showSquare)
+        instruction.classList.add(removeInstruction)
+        resetButton.classList.add(showReset)
     })
 })
-
-
-// mondrianSquares.addEventListener('click', function(){
-//     mondrianSquares.classList.toggle(showSquare)
-// })
