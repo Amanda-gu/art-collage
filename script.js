3c9c94288bd6fe789509c3ef5569c960fa3c8dcb
Amanda-gu <guoj826@newschool.edu>
2026-05-16 14:57:25 -0400

    add reset button

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let showSquare = 'show'
 let mondrianSquares = document.querySelectorAll('#mondrian div')
+let resetButton = document.querySelector('#reset')
+
+resetButton.addEventListener('click', () => {
+    mondrianSquares.forEach((sq) => {
+        sq.classList.remove(showSquare)
+    })
+})
 
 mondrianSquares.forEach((sq) => {
     sq.addEventListener('click', () => {
