7d9db8226803252e042ce1e9611fe2e622bd4a11
Amanda-gu <guoj826@newschool.edu>
2026-05-16 16:07:58 -0400

    added dialog

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let showSquare = 'show'
 let mondrianSquares = document.querySelectorAll('#mondrian div')
+
 let resetButton = document.querySelector('#reset')
 let showReset = 'show-reset'
+
 let instruction = document.querySelector('p')
 let removeInstruction = 'remove-instruction'
+
+let about = document.querySelector('#about')
+let aboutModal = document.querySelector('dialog')
+let closeModal = document.querySelector('#close')
+
 
 resetButton.addEventListener('click', () => {
     mondrianSquares.forEach((sq) => {
@@ -20,4 +27,20 @@ mondrianSquares.forEach((sq) => {
         instruction.classList.add(removeInstruction)
         resetButton.classList.add(showReset)
     })
+})
+
+
+about.addEventListener('click', () => { // “Listen” for clicks.
+	aboutModal.showModal() // This opens it up.
+})
+
+closeModal.addEventListener('click', () => {
+	aboutModal.close() // And this closes it!
+})
+
+document.addEventListener('click', (event) => {
+	// Only clicks on the page itself behind the `dialog`.
+	if (event.target == document.documentElement) {
+		aboutModal.close() // Close it too then.
+	}
 })
